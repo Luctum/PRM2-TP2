b36191d4a0d6c3a45e104fa5f78f59f166e1e12e
Mubanzo Abraham <joseph.mubanzo@etudiant.univ-rennes1.fr>
2019-02-07 10:07:46 +0100

    méthode de creation de vector.

--- vector.py
+++ vector.py
@@ -1,8 +1,19 @@
 from pydash import py_
 
+vector = []
+
 
 def compare(a, b):
-    reala = py_.lower_case(py_.deburr(a))
-    realb = py_.lower_case(py_.deburr(b))
-    return reala == realb and 1 or 0
+    real_a = py_.lower_case(py_.deburr(a))
+    real_b = py_.lower_case(py_.deburr(b))
+    return real_a == real_b and 1 or 0
 
+
+def create_vector(dictionary, comparatee):
+    for element in dictionary:
+        value = compare(element, comparatee)
+        vector.append(value)
+
+
+create_vector(['1', '2', 'e', 'é', '10'], '1')
+print(vector)
